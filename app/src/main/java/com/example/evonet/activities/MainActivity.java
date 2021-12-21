0bc54ed49ef6c0f152477c02ee75452dd644a905
freezing <1473742892@qq.com>
2021-12-21 16:28:01 +0800

    修改主界面布局

--- app/src/main/java/com/example/evonet/activities/MainActivity.java
+++ app/src/main/java/com/example/evonet/activities/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.evonet.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 import com.example.evonet.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivities implements ActivityInterface{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_start);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void bindView() {
+
     }
 }
