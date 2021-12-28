0dd14305074f4290fa6ff2639069c817d247c889
freezing <1473742892@qq.com>
2021-12-28 21:58:34 +0800

    结合GPS定位签到

--- app/src/main/java/com/example/evonet/activities/SignActivity.java
+++ app/src/main/java/com/example/evonet/activities/SignActivity.java
@@ -53,6 +53,7 @@ public class SignActivity extends AppCompatActivity {
     private Record record = new Record();
     private static boolean finishFlag = true;
     private static boolean already = true;
+    private  Location location = null;
 //    private MapView mapView;//初始化全局变量地图组件
 //    private Bundle bundle;//传递签到信息给RecordActivity
 
@@ -70,10 +71,9 @@ public class SignActivity extends AppCompatActivity {
         button_qiandao = (Button) findViewById(R.id.button_qiandao);
         fanhui = (ImageView) findViewById(R.id.back);
         gps = (TextView) findViewById(R.id.show_gps);
-        whether_sign=(TextView)findViewById(R.id.whether_sign);
 
         show_lesson_number=(TextView)findViewById(R.id.show_lesson_number);
-        show_txt=Lesson_Number_Holder.getInstance().getData();
+        show_txt="课程号："+Lesson_Number_Holder.getInstance().getData();
         show_lesson_number.setText(show_txt);//显示课程号
 
 
@@ -89,47 +89,45 @@ public class SignActivity extends AppCompatActivity {
                 intent.putExtra("function","courseSignId");
                 startActivityForResult(intent,0);
 
-//                //GPS定位
-//                LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-////                Toast.makeText(SignIn.this,"签到成功！",Toast.LENGTH_SHORT).show();
-//                if (ActivityCompat.checkSelfPermission(SignActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(SignActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//                    // TODO: Consider calling
-//                    //    ActivityCompat#requestPermissions
-//                    // here to request the missing permissions, and then overriding
-//                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-//                    //                                          int[] grantResults)
-//                    // to handle the case where the user grants the permission. See the documentation
-//                    // for ActivityCompat#requestPermissions for more details.
-//                    return;
-//                }
-//                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-//                        1000,//间隔时间);
-//                        1,//位置间隔
-//                        new LocationListener() {//监听GOS信息是否改变
-//                            @Override
-//                            public void onLocationChanged(@NonNull Location location) {
-//                                //GPS信息发生改变时回调
-//
-//                            }
-//
-//                            @Override
-//                            public void onStatusChanged(String provider, int status, Bundle extras) {
-//                                //GPS状态改变回调
-//                            }
-//
-//                            @Override
-//                            public void onProviderEnabled(@NonNull String provider) {
-//                                //定位提供者启动时回调
-//                            }
-//
-//                            @Override
-//                            public void onProviderDisabled(@NonNull String provider) {
-//                                //定位提供者关闭时回调
-//                            }
-//                        });
-//                Location location=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);//获取最新定位信息
-//                locationUpdaes(location);//将最新定位信息传递给该方法
+                //GPS定位
+                LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+//                Toast.makeText(SignIn.this,"签到成功！",Toast.LENGTH_SHORT).show();
+                if (ActivityCompat.checkSelfPermission(SignActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(SignActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                        1000,//间隔时间);
+                        1,//位置间隔
+                        new LocationListener() {//监听GOS信息是否改变
+                            @Override
+                            public void onLocationChanged(@NonNull Location location) {
+                                //GPS信息发生改变时回调
 
+                            }
+
+                            @Override
+                            public void onStatusChanged(String provider, int status, Bundle extras) {
+                                //GPS状态改变回调
+                            }
+
+                            @Override
+                            public void onProviderEnabled(@NonNull String provider) {
+                                //定位提供者启动时回调
+                            }
+
+                            @Override
+                            public void onProviderDisabled(@NonNull String provider) {
+                                //定位提供者关闭时回调
+                            }
+                        });
+                location=locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);//获取最新定位信息
 
             }
 
@@ -184,6 +182,7 @@ public class SignActivity extends AppCompatActivity {
                                     Toast.makeText(SignActivity.this,"签到失败",Toast.LENGTH_SHORT).show();
                                     finishFlag = false;
                                 }
+                                locationUpdaes(location);//将最新定位信息传递给该方法
                             }
                         }
                     });
@@ -219,18 +218,16 @@ public class SignActivity extends AppCompatActivity {
         {
             StringBuilder stringBuilder=new StringBuilder();
             stringBuilder.append("您处于");
-            stringBuilder.append("经度：");
+            stringBuilder.append("\n经度：");
             stringBuilder.append(location.getLongitude());
-            stringBuilder.append("，纬度：");
+            stringBuilder.append("\n纬度：");
             stringBuilder.append(location.getLatitude());
             gps.setText(stringBuilder.toString());
             button_qiandao.setText("签到成功");
-            button_qiandao.setBackgroundColor(Color.parseColor("#FF03DAC5"));
 
         }else {
             gps.setText("您没有获取到定位信息");
             button_qiandao.setText("签到失败");
-            button_qiandao.setBackgroundColor(Color.parseColor("#ff6347"));
         }
 
     }
